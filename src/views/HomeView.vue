5c373046b23fbb4d2e57086a17373a992aed5581
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-10-27 11:05:27 +0600

    Search input now suggests cities when typed into

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -37,7 +37,19 @@ const getSearchResults = () => {
         	focus:border-weather-secondary focus:outline-none
         	focus:shadow-[0px_1px_0_0_#004E71]">
 
-			
+			<ul 
+				class="absolute bg-weather-secondary text-white 
+				w-full shadow-md py-2 px-1 top-[66px]"
+				v-if="mapboxSearchResults"
+			>
+				<li 
+					v-for="searchResult in mapboxSearchResults" 
+					:key="searchResult.id"
+					class="py-2 cursor-pointer"
+				>
+				{{ searchResult.place_name }}
+				</li>
+			</ul>
 		</div>
 	</main>
 </template>
