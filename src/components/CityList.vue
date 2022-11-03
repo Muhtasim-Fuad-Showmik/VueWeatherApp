1ae6facb439de820999af23301af46f094ad024c
Muhtasim Fuad Showmik <49075537+Muhtasim-Fuad-Showmik@users.noreply.github.com>
2022-11-03 23:57:50 +0600

    Added City List & City Card component

---
+++ src/components/CityList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const savedCities = ref({});
+const getCities = () => {
+    if(localStorage.getItem('savedCities')) {
+        savedCities.value = JSON.parse(localStorage.getItem('savedCities'))
+    };
+
+    const requests = {};
+    savedCities.value.forEach((city) => {
+        requests.push(
+            axios.get(``);
+        );
+    });
+}
+</script>
